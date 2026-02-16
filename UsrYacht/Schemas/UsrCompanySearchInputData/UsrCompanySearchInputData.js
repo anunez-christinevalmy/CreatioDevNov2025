ba9eff16e794302aa4a93a1bb18aab7622293cb4
Angel Abraham Nunez <anunez@christinevalmy.edu>
2026-02-16 01:26:11 -0500

    feat: Add UsrSaveCompanyRecordSubprocess properties and update UsrYacht metadata
    - Created properties.json for UsrSaveCompanyRecordSubprocess with versioning details.
    - Updated descriptor.json in UsrYacht to reflect the latest modification timestamp.
    - Enhanced metadata.json in UsrYacht with new schema properties and boolean flags.
    - Changed IsTrackChangesInDB to true in UsrYacht properties.json for tracking changes.
    - Modified description in UsrYacht descriptor.json to include original maintainer information.

---
+++ UsrYacht/Schemas/UsrCompanySearchInputData/UsrCompanySearchInputData.js
@@ -0,0 +1,93 @@
+define("UsrCompanySearchInputData", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "PageTitle",
+				"values": {
+					"caption": "#MacrosTemplateString(#ResourceString(PageTitle_caption)#)#",
+					"visible": true,
+					"headingLevel": "label"
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "Input_bsbw84j",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PageParameters_UsrTextParameter1_rwtj13d",
+					"control": "$PageParameters_UsrTextParameter1_rwtj13d",
+					"placeholder": "",
+					"tooltip": "",
+					"readonly": false,
+					"multiline": false,
+					"labelPosition": "above"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Input_cskzs11",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 2,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PageParameters_UsrTextParameter1_bds5nxr",
+					"control": "$PageParameters_UsrTextParameter1_bds5nxr",
+					"placeholder": "",
+					"tooltip": "",
+					"readonly": false,
+					"multiline": false,
+					"labelPosition": "above"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 1
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"PageParameters_UsrTextParameter1_rwtj13d": {
+						"modelConfig": {
+							"path": "PageParameters.UsrCompanyName"
+						}
+					},
+					"PageParameters_UsrTextParameter1_bds5nxr": {
+						"modelConfig": {
+							"path": "PageParameters.UsrCountryCode2Letters"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"dataSources": {}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
